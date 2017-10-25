bc4e7713613b589b121218bb82faac5531c1f69f
Bill Erickson <bill.erickson@gmail.com>
2017-10-25 11:06:27 -0500

    make like functional

---
+++ assets/js/src/be-like-content.js
@@ -0,0 +1,50 @@
+// @codekit-prepend "js.cookie.js"
+
+jQuery(function($){
+
+	var cookieName = 'be_like_content';
+	var likedContent = Cookies.get( cookieName );
+	if( ! likedContent )
+		likedContent = [];
+
+	// Set active if already liked
+	$('.be-like-content').each(function(e){
+		var post_id = $(this).data('post-id');
+		console.log( post_id );
+		console.log( likedContent );
+		console.log( likedContent.indexOf( post_id ) );
+		if( likedContent.indexOf( post_id ) != -1 )
+			$(this).addClass('liked');
+	});
+
+	// Like on click
+	var liking = false;
+	$(document).on('click', '.be-like-content', function(e){
+		e.preventDefault();
+		var button = $(this);
+		var post_id = $(button).data('post-id');
+		if( ! liking && -1 == $.inArray( post_id, likedContent ) ) {
+
+			liking = true;
+			$(button).addClass('liking');
+			var data = {
+				action: 'be_like_content',
+				post_id: post_id,
+			};
+			$.post( be_like_content.url, data, function(res){
+				if( res.success ) {
+					$(button).removeClass('liking').addClass('liked').find('.text').html(res.data);
+					var liking = false;
+					likedContent.push( post_id );
+					Cookies.set( cookieName, likedContent, { expires: 365 } );
+					console.log( res );
+				} else {
+					console.log( res );
+				}
+			}).fail(function( xhr, textStatus, e ){
+				console.log( xhr.responseText );
+			});
+		}
+	});
+
+});
--- be-like-content.php
+++ be-like-content.php
@@ -47,6 +47,14 @@ final class BE_Like_Content {
 	private static $instance;
 
 	/**
+	 * Plugin version.
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	private $version = '1.0.0';
+
+	/**
 	 * Settings
 	 *
 	 * @since 1.0.0
@@ -63,9 +71,25 @@ final class BE_Like_Content {
 	public static function instance() {
 		if ( ! isset( self::$instance ) && ! ( self::$instance instanceof BE_Like_Content ) ) {
 			self::$instance = new BE_Like_Content;
+			self::$instance->constants();
 			add_action( 'init', array( self::$instance, 'init' ) );
 		}
 		return self::$instance;
+	}
+
+	/**
+	 * Constants
+	 *
+	 * @since 1.0.0
+	 */
+	function constants() {
+
+		// Version
+		define( 'BE_LIKE_CONTENT_VERSION', $this->version );
+
+		// Directory URL
+		define( 'BE_LIKE_CONTENT_URL', plugin_dir_url( __FILE__ ) );
+
 	}
 
 	/**
@@ -76,6 +100,10 @@ final class BE_Like_Content {
 	function init() {
 
 		$this->settings = apply_filters( 'be_like_content_settings', $this->default_settings() );
+
+		add_action( 'wp_enqueue_scripts',             array( $this, 'scripts' ) );
+		add_action( 'wp_ajax_be_like_content',        array( $this, 'update_count' ) );
+		add_action( 'wp_ajax_nopriv_be_like_content', array( $this, 'update_count' ) );
 	}
 
 	/**
@@ -94,6 +122,42 @@ final class BE_Like_Content {
 	}
 
 	/**
+	 * Scripts
+	 *
+	 * @since 1.0.0
+	 */
+	function scripts() {
+		wp_register_script( 'be-like-content', BE_LIKE_CONTENT_URL . '/assets/js/be-like-content.min.js', array( 'jquery' ), BE_LIKE_CONTENT_VERSION, true );
+		wp_localize_script( 'be-like-content', 'be_like_content', array( 'url' => admin_url( 'admin-ajax.php' ) ) );
+	}
+
+	/**
+	 * Update Count
+	 *
+	 * @since 1.0.0
+	 */
+	function update_count() {
+
+		$post_id = intval( $_POST['post_id'] );
+
+		if( ! $post_id )
+			wp_send_json_error( 'No Post ID' );
+
+		if( !in_array( get_post_type( $post_id ), $this->settings['post_types'] ) )
+			wp_send_json_error( 'This post type does not support likes' );
+
+
+		$count = $this->count( $post_id );
+		$count++;
+		update_post_meta( $post_id, '_be_like_content', $count );
+
+		$data = $this->maybe_count( $this->settings['liked_text'], $post_id, $count );
+		wp_send_json_success( $data );
+
+		wp_die();
+	}
+
+	/**
 	 * Display
 	 *
 	 * @since 1.0.0
@@ -103,6 +167,7 @@ final class BE_Like_Content {
 		if( !in_array( get_post_type(), $this->settings['post_types'] ) )
 			return;
 
+		wp_enqueue_script( 'be-like-content' );
 		echo '<a href="#" class="be-like-content" data-post-id="' . get_the_ID() . '"><span class="text">' . $this->maybe_count( $this->settings['text'], get_the_ID() ) . '</span><span class="hover">' . $this->maybe_count( $this->settings['hover_text'], get_the_ID() ) . '</span></a>';
 	}
 
@@ -111,12 +176,13 @@ final class BE_Like_Content {
 	 *
 	 * @since 1.0.0
 	 */
-	function maybe_count( $text = '', $post_id = '' ) {
+	function maybe_count( $text = '', $post_id = '', $count = false ) {
 
 		if( empty( $text ) || empty( $post_id ) )
 			return $text;
 
-		return str_replace( '{count}', $this->count( $post_id ), $text );
+		$count = $count ? $count : $this->count( $post_id );
+		return str_replace( '{count}', $count, $text );
 	}
 
 	/**
